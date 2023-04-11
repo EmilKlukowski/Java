016621d6028710482c7262d9cf11961ba2dc1523
Emil Klukowski <121125273+EmilKlukowski@users.noreply.github.com>
2023-04-11 14:22:37 +0200

    Create Main.java

---
+++ Frames/Main.java
@@ -0,0 +1,30 @@
+package Frames;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+//            JFrame frame = new JFrame();
+//            frame.setSize(420, 420);
+//            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//            frame.setVisible(true);
+//            frame.setTitle("Gui dzien 1");
+//
+//            Random r = new Random();
+//            frame.getContentPane().setBackground(new Color(
+//                    r.nextInt(256),
+//                    r.nextInt(256),
+//                    r.nextInt(256)
+//            ));
+
+        //to co na gorze rowna sie temu na dole
+        MyFrame myFrame = new MyFrame();
+        //mozna stworzyc bez nazwy new Frame();
+
+
+    }
+}
