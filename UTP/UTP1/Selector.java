d735279bbeb903bc7e77ed8bd32433dc183f9c5e
Emil Klukowski <121125273+EmilKlukowski@users.noreply.github.com>
2023-10-12 11:19:39 +0200

    Create Selector.java

---
+++ UTP/UTP1/Selector.java
@@ -0,0 +1,13 @@
+/**
+ *
+ *  @author Klukowski Emil S27944
+ *
+ */
+
+package zad1;
+
+
+public interface Selector<E> { // Uwaga: interfejs musi być sparametrtyzowany
+    public boolean select(E element);
+
+}  
