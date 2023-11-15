e60b6e568ab170dbe5e36142257b5629b0530358
Emil Klukowski <121125273+EmilKlukowski@users.noreply.github.com>
2023-11-15 14:55:59 +0100

    Update Main.java
    Napisz serwer UDP w języku Java. Użyj do tego otrzymanego wzorca. Serwer powinien oczekiwać na dane.
    
    Po przesłaniu danych od klienta, serwer odbierze wiele datagramów, tak aby łączy rozmiar odebranych danych wyniósł 20 bajtów i wyświetli otrzymane dane na standardowym wyjściu.
    
    Przykładowe dane:
    
    SUCCESS

--- SKJ/Cyberskiller6/Main.java
+++ SKJ/Cyberskiller6/Main.java
@@ -7,7 +7,35 @@ class Main
 {
     public static void main(String[] args) throws IOException
     {
-        DatagramSocket socket = SocketFactory.buildSocket();
+       DatagramSocket socket = SocketFactory.buildSocket();
+
+        byte[] buff = new byte[20];
+        DatagramPacket packet = new DatagramPacket(buff, buff.length);
+
+        try {
+			byte[] buf2 = new byte[20];
+			packet = new DatagramPacket(buf2, 1);
+			String ret = "";
+			for (int i = 0; i < 20; i++) {
+				socket.receive(packet);
+				if(packet.getLength() <= 0){
+					i--;
+					continue;
+				}
+				ret = String.join("", ret, new String(packet.getData(), 0, 1));
+			}
+			
+
+            socket.close();
+            System.out.println(ret);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+        String strout = new String(buff, 0, 20);
+        System.out.println(strout);
+        
+        socket.close();
 
         
     }
